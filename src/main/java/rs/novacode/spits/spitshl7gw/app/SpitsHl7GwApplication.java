8e5e5241152d2221e18736eff7bdb69d37138d15
nikolaspiric <nikolaspiric86@gmail.com>
2019-09-10 12:14:01 +0200

    Initial commit

---
+++ src/main/java/rs/novacode/spits/spitshl7gw/app/SpitsHl7GwApplication.java
@@ -0,0 +1,13 @@
+package rs.novacode.spits.spitshl7gw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpitsHl7GwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpitsHl7GwApplication.class, args);
+	}
+
+}
